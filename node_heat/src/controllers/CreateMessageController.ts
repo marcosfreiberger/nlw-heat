1bb7c8406d58203e7d0c6c25a6586a0c687d2e8f
mfreiber <marcos.freiberger@capgemini.com>
2022-05-17 13:17:26 -0300

    first commit

---
+++ node_heat/src/controllers/CreateMessageController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { CreateMessageService } from './../services/CreateMessageService';
+
+export class CreateMessageController {
+  async handle(request: Request, response: Response) {
+    const { message } = request.body;
+    const { user_id } = request;
+
+    const service = new CreateMessageService();
+
+    try {
+      const result = await service.execute(message, user_id);
+      return response.json(result);
+    } catch (err) {
+      return response.json({ Error: err.message });
+    }
+  }
+}
---
+++ node_heat/src/controllers/ProfileUserController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import { ProfileUserService } from './../services/ProfileUserService';
+
+export class ProfileUserController {
+  async handle(request: Request, response: Response) {
+    const { user_id } = request;
+    const service = new ProfileUserService();
+
+    try {
+      const result = await service.execute(user_id);
+      return response.json(result);
+    } catch (err) {
+      return response.json({ Error: err.message });
+    }
+  }
+}
